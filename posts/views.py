7a67d83f46698dfa5f2aeaf80121ba9edac9eb8b
Nono <75374500+JoseNono13@users.noreply.github.com>
2023-02-15 10:00:10 +0100

    Add files via upload

---
+++ posts/views.py
@@ -0,0 +1,11 @@
+# from django.shortcuts import render
+
+# posts/views.py
+from django.views.generic import ListView # new
+from .models import Post                  # new
+
+
+class HomePageView(ListView):             # new
+    model = Post                          # new
+    template_name = 'home.html'           # new
+    context_object_name = 'all_posts_list'          # new
